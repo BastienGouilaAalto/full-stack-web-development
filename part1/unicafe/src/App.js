47fd743effcf70aaa7f4185b3171bcf69a29b5af
unknown <bastien.gouila@aalto.fi>
2022-09-27 15:53:23 +0300

    part1_11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -10,11 +10,16 @@ const Button = (props) => {
 
 const StatisticsLine = (props) => {
   return(
-    <div>
-        {props.text} {props.value} {props.unitStr}
-      </div>
+    <tr>
+      <td>
+          {props.text}
+      </td>
+      <td>
+          {props.value} {props.unitStr}
+      </td>
+    </tr>
     )
-  }
+}
   
 const Statistics = (props) => {
   console.log(props.clicks)
@@ -27,12 +32,16 @@ const Statistics = (props) => {
   }
     return(
       <div>
-      <StatisticsLine text="good" value={props.clicks[0]}/>
-      <StatisticsLine text="neutral" value={props.clicks[1]}/>
-      <StatisticsLine text="bad" value={props.clicks[2]}/>
-      <StatisticsLine text="all" value={props.clicks[3]}/>
-      <StatisticsLine text="average" value={(props.clicks[0] * 1 + props.clicks[2] * -1) / props.clicks[3]}/>
-      <StatisticsLine text="positive" value={props.clicks[0] / props.clicks[3] * 100} unitStr={'%'}/>
+        <table>
+          <tbody>
+            <StatisticsLine text="good" value={props.clicks[0]}/>
+            <StatisticsLine text="neutral" value={props.clicks[1]}/>
+            <StatisticsLine text="bad" value={props.clicks[2]}/>
+            <StatisticsLine text="all" value={props.clicks[3]}/>
+            <StatisticsLine text="average" value={(props.clicks[0] * 1 + props.clicks[2] * -1) / props.clicks[3]}/>
+            <StatisticsLine text="positive" value={props.clicks[0] / props.clicks[3] * 100} unitStr={'%'}/>
+          </tbody>
+        </table>
       </div>
     )
 }
@@ -63,19 +72,19 @@ const App = () => {
   return (
     <div>
       <h2>give feedback</h2>
-      <Button        
-        onClick={goodReview}        
-        text='good'
-      />      
-      <Button        
-        onClick={neutralReview}        
-        text='neutral'
-      />           
-      <Button        
-        onClick={badReview}
-        text='bad'/>
+        <Button        
+          onClick={goodReview}        
+          text='good'
+        />      
+        <Button        
+          onClick={neutralReview}        
+          text='neutral'
+        />           
+        <Button        
+          onClick={badReview}
+          text='bad'/>
       <h2>statistics</h2>
-      <Statistics clicks={[good, neutral, bad, allClicks]}/>
+        <Statistics clicks={[good, neutral, bad, allClicks]}/>
     </div>
   )
 }
