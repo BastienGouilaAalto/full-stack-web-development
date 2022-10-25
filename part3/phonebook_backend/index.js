2e37d9c474a96f0eb11d114c9de74ce53b5e8a68
unknown <bastien.gouila@aalto.fi>
2022-10-25 18:40:34 +0300

    part3_10

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -109,7 +109,7 @@ app.post('/api/persons', (request, response) => {
     return response.status(200).json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- part3/phonebook_frontend/src/services/Communication.js
+++ part3/phonebook_frontend/src/services/Communication.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://enigmatic-peak-42342.herokuapp.com/api/persons'
 
 const getAllPersons = () => {
   const request = axios.get(baseUrl)
