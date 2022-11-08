f2e4d501a9b62121f46a770b8ae92819c2c73a89
unknown <bastien.gouila@aalto.fi>
2022-11-08 23:11:02 +0200

    part3_15 part3_16

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -25,71 +25,89 @@ app.get('/', (request, response) => {
 
 app.get('/info', (request, response) => {
   const time = new Date()
-  response.send(
-    `<div>
-        <p>Phonebook has info for ${persons.length} people</p>
-    </div>
-    <div>
-        <p>${time}</p>
-    </div>`
-    )
+  Person.find({}).then(persons => {
+    response.send(
+      `<div>
+      <p>Phonebook has info for ${persons.length} people</p>
+      </div>
+      <div>
+      <p>${time}</p>
+      </div>`
+      )
+    })
+  .catch(error => {
+    console.log(error)
+    response.status(500).end()
+  })
 })
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons)
   })
+  .catch(error => {
+    console.log(error)
+    response.status(500).end()
+  })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  
-  if (person) {
-    response.json(person)
-  } 
-  else {
-    response.status(404).end()
-  }})
-
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  if(persons.filter(person => person.id === id).length > 0){
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
-  }
-  else{
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(person => {
+    if (person) {
+      response.json(person)
+    } 
+    else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {  
+app.delete('/api/persons/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response, next) => {  
   const body = request.body
-  console.log(body)
-  if(!body.name){
-    return response.status(400).json({ 
-      error: 'name is missing' 
-    })
+  if (body.name === undefined || body.number === undefined) { 
+    return response.status(400).json({ error: 'content missing' })
   }
-  if(!body.number){
-    return response.status(400).json({
-      error: 'number is missing'
-    })
-  }
-  // if(persons.filter(person => person.name === body.name).length > 0){
-  //   return response.status(400).json({
-  //     error:`${body.name} is already in the phonebook`
-  //   })
-  // }
-    const person = new Person ({
-      name : body.name,
-      number : body.number
-    })
-    person.save().then(savedPerson => 
-    { 
-      return response.status(200).json(savedPerson)
-    })
+
+  const person = new Person ({
+    name : body.name,
+    number : body.number
+  })
+  person.save().then(savedPerson => { 
+    response.json(savedPerson)
+  })
+  .catch(error => next(error))
 })
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+  else if (error.name === 'ValidationError') { 
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+// handler of requests with result to errors
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
