8e0f5b80778b837f880f006da24ca79eff0684f4
unknown <bastien.gouila@aalto.fi>
2022-12-07 10:40:21 +0200

    part4_3

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -2,13 +2,13 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', (request, response) => {
-    Blog.find({}).then(blogs => {
+  Blog.find({}).then(blogs => {
     response.json(blogs)
   })
 })
 
 blogsRouter.get('/:id', (request, response, next) => {
-    Blog.findById(request.params.id)
+  Blog.findById(request.params.id)
     .then(blog => {
       if (blog) {
         response.json(blog)
@@ -20,36 +20,36 @@ blogsRouter.get('/:id', (request, response, next) => {
 })
 
 blogsRouter.post('/', (request, response, next) => {
-    const blog = new Blog(request.body)
+  const blog = new Blog(request.body)
 
-    blog.save()
+  blog.save()
     .then(savedBlog => {
-        response.status(201).json(savedBlog)
+      response.status(201).json(savedBlog)
     })
     .catch(error => next(error))
 })
 
 blogsRouter.delete('/:id', (request, response, next) => {
-    Blog.findByIdAndRemove(request.params.id)
+  Blog.findByIdAndRemove(request.params.id)
     .then(() => {
-    response.status(204).end()
+      response.status(204).end()
     })
     .catch(error => next(error))
 })
 
 blogsRouter.put('/:id', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const blog = {
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes
-    }
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
 
-    Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
     .then(updatedBlog => {
-        response.json(updatedBlog)
+      response.json(updatedBlog)
     })
     .catch(error => next(error))
 })
