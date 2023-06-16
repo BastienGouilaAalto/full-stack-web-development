3bdaeee93b12f6ac071c635e610282d3c6704f4e
unknown <bastien.gouila@aalto.fi>
2023-06-16 14:39:56 +0300

    part5_7

--- part5/bloglist_frontend/src/components/Blog.js
+++ part5/bloglist_frontend/src/components/Blog.js
@@ -1,7 +1,36 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setShowDetails(!showDetails)}>
+          {showDetails ? 'hide' : 'view'}
+        </button>
+      </div>
+      {showDetails && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} 
+            <button>like</button>
+          </div>
+          {typeof blog.user !== 'undefined' ? <div>{blog.user.name}</div>: <div>Unknown user</div>}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
