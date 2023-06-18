9f0ead85e86f1c9094a9543ceff282280400fc69
unknown <bastien.gouila@aalto.fi>
2023-06-18 15:56:49 +0300

    part5_14

--- part5/bloglist_frontend/src/tests/Blog.test.js
+++ part5/bloglist_frontend/src/tests/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import userEvent from '@testing-library/user-event'
 
 import Blog from '../components/Blog'
 
@@ -46,4 +47,31 @@ test("checks that the component displaying a blog renders the blog's title and a
     expect(urlElement).not.toBeInTheDocument();
     expect(likesElement).not.toBeInTheDocument();
   });
-}); 
+
+  test("checks that the blog's URL and number of likes are shown when the button controlling the shown details has been clicked", async () => {
+    // Render the Blog component
+    const component = render(<Blog 
+        blog={blog} 
+        user={user} 
+        updateBlog={mockUpdateBlogHandler} 
+        deleteBlog={mockDeleteBlogHandler} 
+        />);
+    
+    const interactiveUser = userEvent.setup();
+    const button = component.getByText('view');
+    await interactiveUser.click(button);
+
+    // Use the getByText query to check if the title and author are rendered. <div> {blog.title} {blog.author} </div>
+    const titleAndAuthorElement = component.getByText('Test Blog Title Test Author');
+    // Use the queryByText query to check if the URL and number of likes are rendered
+    const urlElement = component.getByText('http://testblog.com');
+    const likesElement = component.getByText('likes 5');
+
+    // Assert that the title and author are rendered
+    expect(titleAndAuthorElement).toBeInTheDocument();
+    expect(urlElement).toBeInTheDocument();
+    expect(likesElement).toBeInTheDocument();
+
+  });
+
+});
