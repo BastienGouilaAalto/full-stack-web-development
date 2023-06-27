57f9bdda36dc9045b161aeacf269bf8d022f16a4
unknown <bastien.gouila@aalto.fi>
2023-06-27 14:14:07 +0300

    part6_12

---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState: 'render here notification...',
+  reducers: {
+    setNotification: (state, action) => {
+      return action.payload;
+    },
+
+    removeNotification: (state, action) => {
+      return '';
+    }
+  },
+})
+
+export const { setNotification, removeNotification } = notificationSlice.actions
+export default notificationSlice.reducer
